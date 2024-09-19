e4b351053b10338087fb2087787fa6b770cd327c
Haider Rehman <haiderrehman500@gmail.com>
2024-09-19 17:40:53 +0500

    service section completed removingn icon

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 var hamBurger = document.querySelector(".hamburger-button");
 
 var phoneMenu = document.querySelector(".list ul")
+
+var menuitem = document.querySelectorAll(".list ul li a");
 hamBurger.addEventListener("click", () => {
     hamBurger.classList.toggle("active");
     phoneMenu.classList.toggle("active");
+});
+
+menuitem.forEach((item) => {
+    item.addEventListener("click", () => { 
+     hamBurger.classList.toggle("active");
+     phoneMenu.classList.toggle("active");
+    });
 });
