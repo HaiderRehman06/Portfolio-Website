e66a7e69e44783c06eb889c0d9bac273cbae19c8
Haider Rehman <haiderrehman500@gmail.com>
2024-09-19 21:46:53 +0500

    adding project section and images and header animation

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var hamBurger = document.querySelector(".hamburger-button");
 var phoneMenu = document.querySelector(".list ul")
 
 var menuitem = document.querySelectorAll(".list ul li a");
+
+var header = document.querySelector(".head-container");
 hamBurger.addEventListener("click", () => {
     hamBurger.classList.toggle("active");
     phoneMenu.classList.toggle("active");
@@ -13,4 +15,13 @@ menuitem.forEach((item) => {
      hamBurger.classList.toggle("active");
      phoneMenu.classList.toggle("active");
     });
+});
+
+document.addEventListener("scroll", () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.style.backgroundColor = "#29393c";
+    }else {
+        header.style.backgroundColor = "transparent";
+    }
 });
